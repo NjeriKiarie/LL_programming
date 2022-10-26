ca900e9247a91da0cb5bb6abeeecdd4ae4f0b951
NjeriKiarie <kcarolnjeri@gmail.com>
2022-10-26 13:12:41 -0700

    Dont hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _atoi - a function that convert a strng to an integer
+ * @s: input
+ *
+ * Return: Always 0
+ */
+
+int _atoi(char *s)
+{
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - generates random valid passwords for the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	return (0);
+}
