cf0d3f07fc533c179161610ec728903b5687d0d1
NjeriKiarie <kcarolnjeri@gmail.com>
2022-10-28 02:24:21 -0700

    there are only 3 colors

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ *
+ * Returns: The value of the last digit
+ */
+
+int print_last_digit(int l)
+{
+	if (l < 0)
+		l = l * -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
