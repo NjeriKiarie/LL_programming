6f7f2e78b9bbe6f430dcfd08fb56d0a4e29f4e6b
NjeriKiarie <kcarolnjeri@gmail.com>
2022-11-09 03:45:49 -0800

    It aint what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints my name
+ *
+ * @argc: counts the number of parameters
+ * @argv: Pointer of array of pointers 
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (agrc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
