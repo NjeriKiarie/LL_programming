7af2db2e5bb271b249232e7ef06b61bb556d0cdd
NjeriKiarie <kcarolnjeri@gmail.com>
2023-02-26 01:41:36 -0800

    How many Legs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,14 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: pet name
- * @age: pet age
- * @owner: pet owner
- */
-
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
 struct dog
 {
 	char *name;
@@ -16,17 +16,13 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * dog - rename strucut with typedef
- */
-
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
